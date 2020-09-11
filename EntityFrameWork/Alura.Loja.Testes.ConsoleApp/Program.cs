c6a9d2bc58cdb69cb40f794beb3ad98876b83c3d
Daniel Bonifacio Romera <danielbr@cit>
2020-09-10 21:51:10 -0300

    Aprendendo Entity e comparando com o DAO

---
+++ EntityFrameWork/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // GravarUsandoAdoNet();
+            GravandoUsandoEntity();
+        }
+
+        private static void GravandoUsandoEntity()
+        {
+            Produto p = new Produto();
+            p.Nome = "Harry Potter e a Ordem da Fênix";
+            p.Categoria = "Livros";
+            p.Preco = 19.89;
+
+            using (var contexto= new LojaContext()) //Uma convenção utilizada é o uso da palavra context para referenciar o contexto
+            {                                       //Essa classe possui o conceito de persistir todas as classes que necessitam
+                contexto.Produtos.Add(p);           //ser persistidas ela é generica servindo para Update, Insert, Select e Delete   
+                contexto.SaveChanges();
+            }
+        }
+
+
+        //private static void GravarUsandoAdoNet()
+        //{
+        //    Produto p = new Produto();
+        //    p.Nome = "Harry Potter e a Ordem da Fênix";
+        //    p.Categoria = "Livros";
+        //    p.Preco = 19.89;
+
+        //    using (var repo = new ProdutoDAO()) //Neste caso é representado por uma classe chamada ProdutoDAO 
+        //    {
+        //        repo.Adicionar(p);
+        //    }
+        //}
+    }
+}
