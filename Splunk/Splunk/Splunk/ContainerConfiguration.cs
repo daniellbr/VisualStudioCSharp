a61c480981d37c8040edbf4cedacc17af609d119
Daniel Bonifacio Romera <danielbr@cit>
2020-09-10 21:53:04 -0300

    Poc com a utilizacao do SPLUNK

---
+++ Splunk/Splunk/Splunk/ContainerConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NLog.Extensions.Logging;
+using Microsoft.Extensions.Logging;
+using System;
+using NLog;
+using NLog.Common;
+
+namespace Splunk
+{
+    internal static class ContainerConfiguration
+    {
+        public static IServiceProvider Configure()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            LogManager.LoadConfiguration("nlog.xml");
+            InternalLogger.LogToConsole = true;
+            InternalLogger.LogLevel = NLog.LogLevel.Trace;
+
+            serviceCollection.AddLogging(log =>
+            {
+                log.ClearProviders();
+                log.AddNLog();
+            }).Configure<LoggerFilterOptions>(conf => conf.MinLevel = Microsoft.Extensions.Logging.LogLevel.Trace);
+
+            return serviceCollection.BuildServiceProvider();
+        }
+    }
+}
---
+++ Splunk/Splunk/Splunk/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Splunk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = ContainerConfiguration.Configure();
+
+            var logger = serviceProvider.GetService<ILogger<Program>>();
+
+            while (true)
+            {
+                logger.LogInformation($"Teste chamada Api Splunk {DateTime.Now}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
