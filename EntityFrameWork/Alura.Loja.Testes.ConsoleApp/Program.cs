595ee837267eb0bfff6c7c330f0c1b682a1c2047
Daniel Bonifacio Romera <danielbr@cit>
2020-09-12 17:25:17 -0300

    Fazendo o select utilizando o Entity

--- EntityFrameWork/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ EntityFrameWork/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -11,19 +11,36 @@ namespace Alura.Loja.Testes.ConsoleApp
         static void Main(string[] args)
         {
             // GravarUsandoAdoNet();
-            GravandoUsandoEntity();
+            // GravandoUsandoEntity();
+            RecuperarProdutos();
         }
 
+        public static void RecuperarProdutos()
+        {
+            using (var repoConsulta = new LojaContext())
+            {
+                //IList<Produto> produtos = repoConsulta.Produtos.ToList();
+                var produtos = repoConsulta.Produtos.ToList();
+
+                foreach (var item in produtos)
+                {
+                    Console.WriteLine(item.Nome);
+                }
+                Console.ReadLine();
+            }
+        }
         private static void GravandoUsandoEntity()
         {
             Produto p = new Produto();
-            p.Nome = "Harry Potter e a Ordem da Fênix";
+            p.Nome = "Super Chef";
             p.Categoria = "Livros";
-            p.Preco = 19.89;
+            p.Preco = 11.55;
 
-            using (var contexto= new LojaContext()) //Uma convenção utilizada é o uso da palavra context para referenciar o contexto
+
+
+            using (var contexto = new LojaContext())//Uma convenção utilizada é o uso da palavra context para referenciar o contexto
             {                                       //Essa classe possui o conceito de persistir todas as classes que necessitam
-                contexto.Produtos.Add(p);           //ser persistidas ela é generica servindo para Update, Insert, Select e Delete   
+                contexto.Produtos.Add(p);           //ser persistidas ela é generica servindo para Update, Insert, Select e Delete               
                 contexto.SaveChanges();
             }
         }
