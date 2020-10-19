e6bd5fb463dc2b16ae36077b41c417a4720825e7
Daniel Bonifácio Romera <dbromera@gmail.com>
2020-10-19 08:38:38 -0300

    Configurando segurança utilizando JWT inclusão de duas dependencias System.IdentityModel.Tokens.Jwt e Microsoft.AspNetCore.Authentication.JwtBearer alem da configuração no Startup

--- Alura.WebAPI_I/Alura.WebAPI/Alura.WebAPI.WebApp/Startup.cs
+++ Alura.WebAPI_I/Alura.WebAPI/Alura.WebAPI.WebApp/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Alura.WebAPI.WebApp.Formatters;
+using Microsoft.IdentityModel.Tokens;
+using System;
 
 namespace Alura.ListaLeitura.WebApp
 {
@@ -48,7 +50,26 @@ namespace Alura.ListaLeitura.WebApp
             {
                 options.OutputFormatters.Add(new LivroCsvFormatter()); //sobrecarga no metodo para configurar a opcao para criar uma lista nova de tipo de formatação
             }).AddXmlDataContractSerializerFormatters(); //Alem de Json ele irá entregar XML também
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = "JwtBearer"; //Criando as configurações do "Portador" Quem solicita
+                options.DefaultChallengeScheme = "JwtBearer"; 
+            }).AddJwtBearer("JwtBearer", optins => {
+                optins.TokenValidationParameters = new TokenValidationParameters //Para validação do token são necessários os parametros abaixo
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8
+                                                                .GetBytes("alura-webapi-authentication-valid")), //chave para validar no Emitente ou o dono da informação
+                    ClockSkew = TimeSpan.FromMinutes(5), //tempo para expirar esta chave em caso de não utilização
+                    ValidIssuer = "Alura.WebApp", //Aplicação que pode acessar a "aplicação"
+                    ValidAudience = "Postaman",
+                };
+            });
         }
+
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
