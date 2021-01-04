5853d8abd4c2adf513276becac69486aca3936da
Daniel Bonifácio Romera <dbromera@gmail.com>
2021-01-04 20:56:37 -0300

    Criação do projeto e criação das Entidades Produto, Fornecedor e  Endereco

---
+++ AppMVCBasica/AppMVCBasica/Models/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AppMVCBasica.Models
+{
+    public abstract class Entity
+    {
+        protected Guid id { get; set; }
+
+        public Entity()
+        {
+            id = Guid.NewGuid();
+        }
+    }
+}
---
+++ AppMVCBasica/AppMVCBasica/Models/Fornecedor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace AppMVCBasica.Models
+{
+    public partial class Fornecedor : Entity
+    {
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+        public TipoFornecedor TipoFornecedor { get; set; }
+        public Endereco Endereco { get; set; }
+        public bool Ativo { get; set; }
+
+        /* EF Relations 1 => N  */
+
+        public IEnumerable<Produto> Produtos { get; set; }
+    }
+}
