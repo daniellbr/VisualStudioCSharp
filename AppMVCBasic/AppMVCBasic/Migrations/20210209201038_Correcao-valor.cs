41ee601d2c60b57103162b7f212851667e1d1a87
Daniel Bonifácio Romera <dbromera@gmail.com>
2021-02-09 19:12:27 -0300

    MVC Basica funcionando e gravando no BD

---
+++ AppMVCBasic/AppMVCBasic/Migrations/20210209201038_Correcao-valor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AppMVCBasic.Migrations
+{
+    public partial class Correcaovalor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Valor",
+                table: "Produtos",
+                maxLength: 18,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(18)",
+                oldMaxLength: 18);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Valor",
+                table: "Produtos",
+                type: "nvarchar(18)",
+                maxLength: 18,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldMaxLength: 18);
+        }
+    }
+}
---
+++ AppMVCBasic/AppMVCBasic/Models/TipoFornecedor.cs
@@ -0,0 +1,8 @@
+﻿namespace AppMVCBasic.Models
+{
+    public enum TipoFornecedor 
+    { 
+        PessoaFisica = 1,
+        PessoaJuridica = 2
+    }
+}
