eddacc8068821f075b83a3084c3801dd45a685c4
Daniel Bonifácio Romera <dbromera@gmail.com>
2021-02-28 20:57:56 -0300

    Limpeza da StartUp criando classes dedicadas dentro da pasta Configurations e sendo chamadas na StarUp de forma mais limpa...

---
+++ AppMvcCompleta/src/DevIO.App/Configurations/DependencyInjectionConfig.cs
@@ -0,0 +1,21 @@
+﻿using DevIO.App.Extensions;
+using DevIO.Business.Interfaces;
+using DevIO.Data.Repository;
+using Microsoft.AspNetCore.Mvc.DataAnnotations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevIO.App.Configurations
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+            services.AddSingleton<IValidationAttributeAdapterProvider, MoedaValidationAttributeAdapterProvider>();
+
+            return services;
+        }
+    }
+}
