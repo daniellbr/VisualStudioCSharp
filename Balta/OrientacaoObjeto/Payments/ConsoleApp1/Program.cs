0f5e6a86cb8cc8bd191068f8201632dd8f6a3159
Daniel Bonifácio Romera <dbromera@gmail.com>
2022-12-25 19:32:05 -0300

    Conceito de herança na orientação a objeto

---
+++ Balta/OrientacaoObjeto/Payments/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pagamentoBoleto = new PagamentoBoleto();
+            pagamentoBoleto.Vencimento = DateTime.Now;
+            pagamentoBoleto.DadosBoleto = "Pago";
+            pagamentoBoleto.Pagar();
+
+            var pagamentoCartao = new PagamentoCartao();
+            pagamentoCartao.DadosCartao = "Cartao Black";
+            pagamentoCartao.Vencimento = DateTime.Now;
+            pagamentoCartao.Pagar();
+
+            var pagamento = new Pagamento();
+            pagamento.Vencimento = DateTime.Now;
+            pagamento.Pagar();
+        }
+    }
+
+    class Pagamento
+    {
+        //Propriedades
+        public DateTime Vencimento;
+
+        //Métodos ou funções do objeto
+        public void Pagar()
+        {
+
+        }
+       
+    }
+
+    class PagamentoBoleto : Pagamento
+    {
+        public string DadosBoleto;
+    }
+
+    class PagamentoCartao : Pagamento
+    {
+        public string DadosCartao;
+    }
+
+
+}
