fc40041bcdf83dac7779076a3b29ecaad8072b98
Daniel Bonifácio Romera <dbromera@gmail.com>
2023-03-12 10:42:00 -0300

    Implementando Interface

---
+++ Balta/OrientacaoObjeto/Interface/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public interface IPagamento
+        {
+            DateTime HoraPagamento { get; set; }
+
+            void Pagar(double valorPagamento);
+        }
+
+        public class Pagamento : IPagamento
+        {
+            public DateTime HoraPagamento { get; set; }
+
+            public void Pagar(double valor)
+            {
+
+            }
+        }
+    }
+}
