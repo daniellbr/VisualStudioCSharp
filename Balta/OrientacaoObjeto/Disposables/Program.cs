1765d50e36d88813e2c115dfee7bd867ea82afcf
Daniel Bonifácio Romera <dbromera@gmail.com>
2023-03-11 21:43:28 -0300

    Implementando o Dispose

---
+++ Balta/OrientacaoObjeto/Disposables/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Disposables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var dispor = new Dispor();
+            dispor.Dispose();
+
+            using (var disporComUsing = new Dispor())
+            {
+                Console.WriteLine("Abrindo a conexão mas fechando sem chamar o dispose");
+            };
+        }
+
+        public class Dispor : IDisposable
+        {
+            public Dispor()
+            {
+                Console.WriteLine("Abrindo a conexão");
+            }
+
+            public void Dispose()
+            {
+                Console.WriteLine("Finalizando a conexão");
+            }
+        }
+    }
+}
