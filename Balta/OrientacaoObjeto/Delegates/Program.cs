de15e047b7e8201bb47f8f83b3de480da44c5623
Daniel Bonifácio Romera <dbromera@gmail.com>
2023-03-18 21:17:28 -0300

    Implementando Events com Delegate

--- Balta/OrientacaoObjeto/Delegates/Program.cs
+++ Balta/OrientacaoObjeto/Delegates/Program.cs
@@ -4,9 +4,23 @@ namespace Delegates
 {
     class Program
     {
+        public static void RealizarPagamento(double valor)
+        {
+            Console.WriteLine($"Pago valor de {valor}");
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var pagar = new Pagamento.Pagar(RealizarPagamento);
+
+            pagar(25);
         }
+    }
+
+    public class Pagamento
+    {
+        public delegate void Pagar(double valor);
+
     }
 }
