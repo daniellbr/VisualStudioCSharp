845d146de2ab1fa9d28f55e90041afb202f1c5fd
Daniel Bonifácio Romera <dbromera@gmail.com>
2023-03-19 19:51:07 -0300

    Implementando classes genericas

---
+++ Balta/OrientacaoObjeto/GenericsI/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace GenericsI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var person = new Person();
+            var payment = new Payment();
+            var context = new DataContext<Person>();
+            context.Save(person);
+
+            //Como o context foi criado do tipo Person, colocando o context payment não roda
+            //context.Save(payment);
+
+        }
+
+        public class DataContext<T>
+        {
+            public void Save(T entity)
+            {
+
+            }
+        }
+
+        public class Person
+        {
+
+        }
+
+        public class Payment
+        {
+
+        }
+
+        public class Subscription
+        {
+
+        }
+
+    }
+}
