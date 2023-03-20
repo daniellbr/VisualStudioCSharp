ded3a7b373629c6f9bd1e171a98c90700fc42d2d
Daniel Bonifácio Romera <dbromera@gmail.com>
2023-03-20 08:55:58 -0300

    Ajuste do nome da classe para a sobrecarga

--- Balta/OrientacaoObjeto/EventGenerics/Program.cs
+++ Balta/OrientacaoObjeto/EventGenerics/Program.cs
@@ -9,8 +9,8 @@ namespace EventGenerics
             var room = new Room(3);
 
             //Delegação do evento... Toda vez que o Evento RoomSoudOutEvent ocorres ele chama o 
-            //metodo OnRoomSoudOutActivate via DELEGATE que irá disparar a mensagem de sala lotada
-            room.RoomSoudOutEvent += OnRoomSoudOutActivate;
+            //metodo OnRoomSoudOut via DELEGATE que irá disparar a mensagem de sala lotada
+            room.RoomSoudOutEvent += OnRoomSoudOut;
             room.ResertSeat();
             room.ResertSeat();
             room.ResertSeat();
@@ -18,11 +18,10 @@ namespace EventGenerics
             room.ResertSeat();
             room.ResertSeat();
             room.ResertSeat();
-
-
         }
 
-        static void OnRoomSoudOutActivate(object sender, EventArgs e)
+
+        static void OnRoomSoudOut(object sender, EventArgs e)
         {
             Console.WriteLine("Sala lotada!");
         }
@@ -41,7 +40,7 @@ namespace EventGenerics
                 SeatsInUse++;
                 if (SeatsInUse > Seats)
                 {
-                    OnRoonSoudOut(EventArgs.Empty);
+                    OnRoomSoudOut(EventArgs.Empty);
                 }
                 else
                 {
@@ -49,11 +48,11 @@ namespace EventGenerics
                 }
             }
 
-            //Deinição do evento RooSoudOutEvent
+            //Definição do evento RooSoudOutEvent
             public event EventHandler RoomSoudOutEvent;
 
             //A implementação Base do evento OnRoomSoudOut
-            protected virtual void OnRoonSoudOut(EventArgs eventArgs)
+            protected virtual void OnRoomSoudOut(EventArgs eventArgs)
             {
                 EventHandler handler = RoomSoudOutEvent;
                 handler?.Invoke(this, eventArgs);
