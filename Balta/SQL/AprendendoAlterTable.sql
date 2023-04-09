46a63768adf8be90409399c48bd6c8e747a114ca
Daniel Bonifácio Romera <dbromera@gmail.com>
2023-04-09 11:31:36 -0300

    Aprendendo a utilizacao do UNIQUE

--- Balta/SQL/AprendendoAlterTable.sql
+++ Balta/SQL/AprendendoAlterTable.sql
@@ -12,3 +12,10 @@ ALTER TABLE [Aluno]
 
 DROP TABLE Aluno
 
+CREATE TABLE [Aluno](
+    [Id] INT NOT NULL,
+    [Nome] NVARCHAR(80) NOT NULL,
+    [Email] NVARCHAR(180) NOT NULL UNIQUE,
+    [Nascimento] DATETIME NULL,
+    [Active] BIT DEFAULT(0)
+)
